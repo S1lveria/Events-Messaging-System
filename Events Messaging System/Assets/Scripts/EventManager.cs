a4373cb041c4785463c62dd1129c3adfc13445e9
Ivan <S1lveria@users.noreply.github.com>
2017-12-17 20:36:09 -0500

    Create a scene and EventManager script

---
+++ Events Messaging System/Assets/Scripts/EventManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    private Dictionary<string, UnityEvent> eventDictionary;
+
+    private static EventManager eventManager; 
+
+    public static EventManager instance
+    {
+        get
+        {
+            if(!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager));
+            }
+        }
+    }
+}
