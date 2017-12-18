f4f8b252af9c7e510578f9bae19d4301cd3bc066
Ivan <S1lveria@users.noreply.github.com>
2017-12-18 18:41:07 -0500

    Develop the eventManager
    - Initialize eventManager
    - Create Start- and Stop-Listeners for the events

--- Events Messaging System/Assets/Scripts/EventManager.cs
+++ Events Messaging System/Assets/Scripts/EventManager.cs
@@ -22,9 +22,41 @@ public class EventManager : MonoBehaviour
                 }
                 else
                 {
- //                   eventManager, Initialize this
+					eventManager.Init ();
                 }
             }
         }
     }
+
+	void Init() 
+	{
+		if (eventDictionary == null)
+		{
+			eventDictionary = new Dictionary<string, UnityEvent>();
+		}
+	}
+
+	public static void StartListening(string eventName, UnityAction listener) 
+	{
+		UnityEvent thisEvent = null;
+		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent)) {
+			thisEvent.AddListener (listener);
+		} 
+		else 
+		{
+			thisEvent = new UnityEvent ();
+			thisEvent.AddListener (listener);
+			instance.eventDictionary.Add (eventName, thisEvent);
+		}
+	}
+
+	public static void StopListening(string eventName, UnityAction listener) 
+	{
+		if (eventManager == null) return;
+		UnityEvent thisEvent = null;
+		if (instance.eventDictionary.TryGetValue (eventName, out thisEvent)) 
+		{
+			thisEvent.RemoveListener (listener);
+		}
+	}
 }
